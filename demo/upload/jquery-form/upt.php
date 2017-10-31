7e1ea506ca06876cba624e7961f6e253b411b57b
youngjuning <1003719811@qq.com>
2017-10-31 08:58:29 +0800

    调整异步上传文件

--- demo/upload/jquery-form/upt.php
+++ demo/upload/jquery-form/upt.php
@@ -1,34 +1,48 @@
 <?php
-  @$fileInfo=$_FILES['file'];
-  $filename=$fileInfo['name'];
-  $type=$fileInfo['type'];
-  $tmp_name=$fileInfo['tmp_name'];
-  $size=$fileInfo['size'];
-  $error=$fileInfo['error'];
-  $maxSize=8388608;//文件大小限制(1024*1024*xMB)
-  $allowExt=array('jpeg','jpg','png','gif');//文件扩展名限制
+  if (!isset($_FILES['file'])||empty($_FILES['file'])) {
+    echo '{"success":false,"msg":"没有选择文件！"}';
+    return false;
+  }
+  if (!isset($_POST['dir'])||empty($_POST['dir'])) {
+    echo '{"success":false,"msg":"没有选择文件夹！"}';
+    return false;
+  }
+
+  $file=$_FILES['file']; // 获取上传文件的全部信息
+  $filename=$file['name']; // 上传文件的名称
+  $type=$file['type']; // 上传文件的MIME类型
+  $tmp_name=$file['tmp_name']; // 上传到服务器上的临时文件名
+  $size=$file['size']; // 上传文件的大小
+  $error=$file['error']; // 上传文件的错误号
+  $maxSize=8388608;//文件大小限制8M(1M=1024*1024*Byte)
+  $allowExt=['jpeg','jpg','png','gif'];//文件扩展名限制
+
+  $_dir = $_POST['dir'];
 
   //检查是否有选择文件
-  if ($error==UPLOAD_ERR_OK){
+  if ($error===0){ //UPLOAD_ERR_OK
     //上传限制
-    if ($fileInfo['size']>$maxSize) {
+    if ($size>$maxSize) {
       echo '{"success":false,"msg":"上传文件大小超过设定值"}';
-      return;
+      return false;
     }
-    $ext=pathinfo($fileInfo['name'],PATHINFO_EXTENSION);
+
+    // 检测扩展名
+    $ext=pathinfo($filename,PATHINFO_EXTENSION);
     if (!in_array($ext,$allowExt)) {
       echo '{"success":false,"msg":"文件错误或损坏无法读取！"}';
-      return;
+      return false;
     }
-    //检查是否通过HTTP POS方式上传
-    if (!is_uploaded_file($fileInfo['tmp_name'])) {
-      echo '{"success":false,"msg":"上传错误！请使用HTTP POS方式上传文件。"}';
-      return;
+
+    //检查是否通过HTTP POST方式上传
+    if (!is_uploaded_file($tmp_name)) {
+      echo '{"success":false,"msg":"上传错误！请使用HTTP POST方式上传文件。"}';
+      return false;
     }
 
     //输出提示
-    if (move_uploaded_file($tmp_name, "upload_folder/".$filename)) {
-      echo '{"success":true,"msg":"文件：“'.$filename.'”上传成功！'.$type.'"}';
+    if (move_uploaded_file($tmp_name, $_dir."/".$filename)) {
+      echo '{"success":true,"msg":"文件：“'.$filename.'”上传成功！'.$type.'-扩展名：-'.pathinfo($filename,PATHINFO_EXTENSION).'"}';
     }else{
       echo '{"success":false,"msg":"文件：'.$filename.'上传失败"}';
     }
