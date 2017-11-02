88f2bedb30dfec6641ab937507647b3ffbad6de7
youngjuning <1003719811@qq.com>
2017-11-02 18:42:01 +0800

    整理gulp插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,25 @@
-// 引入gulp
-var gulp = require('gulp');
-// 引入压缩js的包
-var uglify = require('gulp-uglify');
-// 引入压缩css的包
-var cssnano = require('gulp-cssnano');
-// 引入合并文件的包
-var concat = require('gulp-concat');
-// 引入压缩html的包
-const htmlmin = require('gulp-htmlmin');
-// 引入编译pug的包
-var pug = require('gulp-pug');
+var gulp = require('gulp'); // 引入gulp
+var uglify = require('gulp-uglify'); // 引入压缩js的包
+var cssnano = require('gulp-cssnano'); // 引入压缩css的包
+var concat = require('gulp-concat'); // 引入合并文件的包
+var htmlmin = require('gulp-htmlmin'); // 引入压缩html的包
+var pug = require('gulp-pug'); // 引入编译pug的包
+
 gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
-  console.log('测试');
+  gulp.watch(['./template/index.pug'],['pug']);
+  gulp.watch(['./assets/less/*.less'],['less']);
 });
 
-gulp.task('script', function() {
+// 压缩JS
+gulp.task('js', function() {
   // 把当前文件夹下面的app.js做一个压缩处理，处理完之后放到dist文件夹里面
   gulp.src('./app.js')
     .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('pug',function(){
-  gulp.src('./template/index.pug')
-    .pipe(pug({
-      pretty:true
-    }))
-    .pipe(gulp.dest('./')); // dest:destination
-});
-
+// 压缩HTML
 gulp.task('htmlmin',function () {
-  // 把index.html压缩 --> dist
   gulp.src('./index.html')
     .pipe(htmlmin({
       collapseWhitespace:true,
@@ -41,7 +29,24 @@ gulp.task('htmlmin',function () {
     .pipe(gulp.dest('./dist'));
 });
 
-// gulp.watch 监视文件变化，执行响应任务
-gulp.task('dog',function(){
-  gulp.watch('./template/index.pug',['pug'])
+// pug编译
+gulp.task('pug',function(){
+  gulp.src('./template/index.pug')
+    .pipe(pug({
+      pretty:true
+    }))
+    .pipe(gulp.dest('./')); // dest:destination
+});
+
+
+//编译Less
+var less = require('gulp-less'); // 引入编译less的包
+var path = require('path');
+
+gulp.task('less', function () {
+  gulp.src('./assets/less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./assets/css/'));
 });
